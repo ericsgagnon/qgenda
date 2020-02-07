3b752fee8db5f09f1f2e9df49afc277ae376cd2a
Eric <ericsgagnon@gmail.com>
2020-02-07 03:58:50 -0500

    WIP

--- main.go
+++ main.go
@@ -63,6 +63,20 @@ func main() {
 	}
 	// fmt.Println(q)
 	q.Login()
+	// fmt.Println(q.Authorization.Get(http.CanonicalHeaderKey("Authorization")))
+	// ck := q.Client.Jar.Cookies(&url.URL{
+	// 	Scheme: "https",
+	// 	Host:   "api.qgenda.com",
+	// })
+
+	// tt := *q.BaseURL
+	// tt.Path = ""
+	// ck := q.Client.Jar.Cookies(&tt)
+
+	// for _, v := range q.Client.Jar.Cookies(q.BaseURL) {
+	for _, v := range q.Client.Jar.Cookies(&url.URL{}) {
+		fmt.Printf("%v: %v\n", v.Name, v.Value)
+	}
 
 }
 
@@ -70,6 +84,8 @@ func main() {
 // bearer header in the client with the returned access token
 func (q *QgendaClient) Login() {
 
+	//fmt.Println(q.Authorization.Get(http.CanonicalHeaderKey("Authorization")))
+	//TODO: check for Auth cookie or header, get another if missing or expired
 	// request URL
 	reqURL := *q.BaseURL
 	reqURL.Path = path.Join(reqURL.Path, "/login")
@@ -94,29 +110,39 @@ func (q *QgendaClient) Login() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	// fmt.Println(resData["access_token"])
-	// for k, v := range resData {
-	// 	fmt.Println("-----------------------")
-	// 	fmt.Printf("%#v: %#v\n", k, v)
-	// }
 
 	// Set the Authorization header in the QgendaClient
 	q.Authorization.Set(
 		http.CanonicalHeaderKey("Authorization"),
 		fmt.Sprintf("bearer %v", resData["access_token"]),
 	)
-	ac := &http.Cookie{
-		Name:   http.CanonicalHeaderKey("Authorization"),
-		Value:  fmt.Sprintf("bearer %v", resData["access_token"]),
-		Domain: q.BaseURL.Hostname(),
-		// Expires: time.Now().Add(resData["expires_in"] * time.Second),
-		Expires: time.Now().Add(validDuration),
+
+	ac := []*http.Cookie{
+		&http.Cookie{
+			Name:     http.CanonicalHeaderKey("Authorization"),
+			Value:    fmt.Sprintf("bearer %v", resData["access_token"]),
+			Domain:   q.BaseURL.Hostname(),
+			Path:     "/",
+			SameSite: http.SameSiteLaxMode,
+			// Expires: time.Now().Add(resData["expires_in"] * time.Second),
+			Expires: time.Now().Add(validDuration),
+		},
 	}
-	fmt.Println(ac)
-	// q.Client.Jar.SetCookies(q.BaseURL, )
-	// fmt.Println(q.Authorization)
+
+	// for _, v := range ac {
+	// 	fmt.Printf("%v: %v\n", v.Name, v.Value)
+	// }
+	q.Client.Jar.SetCookies(q.BaseURL, ac)
 }
 
+// fmt.Sprintln(ac)
+// q.Client.Jar.SetCookies(q.BaseURL)
+// fmt.Println(q.Authorization)
+// fmt.Println(resData["access_token"])
+// for k, v := range resData {
+// 	fmt.Println("-----------------------")
+// 	fmt.Printf("%#v: %#v\n", k, v)
+// }
 // request body
 // v := &url.Values{}
 // v.Add("email", q.Email)
